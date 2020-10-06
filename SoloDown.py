f31e7767c7113cead018ac56de536e326bbfd49e
space9bug <1392669598@qq.com>
2020-10-06 14:57:47 +0800

    add thread processing

--- SoloDown.py
+++ SoloDown.py
@@ -2,6 +2,7 @@ import os
 import re
 import subprocess
 import sys
+import threading
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import ttk
@@ -25,6 +26,7 @@ class Application(tk.Tk):
         self.program_name = "自嗨音频下载器"
         self.show_screen = tk.StringVar()
         self.show_progress = tk.StringVar()
+        self.lock = threading.Lock()
         self.createWidgets()
 
     def createWidgets(self):
@@ -78,79 +80,93 @@ class Application(tk.Tk):
         labDeclare = tk.Label(self, text=declare_str, font=("宋体", 9), fg="red")
         labDeclare.place(x=0, y=285, width=500, height=25)
 
+    def down_thread(self, music_url):
+        self.lock.acquire()
+        try:
+            self.progressBar["value"] = 0
+            self.show_progress.set("")
+            self.show_screen.set(self.program_name)
+            self.update()
+            try:
+                music_data = Amusic.get_all_music_parm(music_url)
+                if music_data[0] == "null" and music_data[1] == "null":
+                    print("不支持此分享链接或者链接格式错误")
+                    messagebox.showwarning(title="警告", message="不支持此分享链接或者链接格式错误")
+                    return
+                music_name = music_data[0]
+                music_play_url = music_data[1]
+                self.show_screen.set(music_name)
+                ###开始下载###
+                try:
+                    if sys.platform[:3] == "win":
+                        down_path = "downloads/"
+                    if sys.platform == "darwin":
+                        down_path = os.path.expanduser("~/Music/")
+                    down_name = music_name + ".temp"
+                    str_out = ['./aria2c', '-d', down_path, '-o', down_name, music_play_url]
+                    print(str_out)
+                    if sys.platform[:3] == "win":
+                        si = subprocess.STARTUPINFO()
+                        si.dwFlags = subprocess.CREATE_NEW_CONSOLE | subprocess.STARTF_USESHOWWINDOW
+                        si.wShowWindow = subprocess.SW_HIDE
+                        process = subprocess.Popen(str_out, shell=False, stdout=subprocess.PIPE,
+                                                   stderr=subprocess.STDOUT,
+                                                   encoding="utf-8",
+                                                   text=True, startupinfo=si)
+                    if sys.platform == "darwin":
+                        process = subprocess.Popen(str_out, shell=False, stdout=subprocess.PIPE,
+                                                   stderr=subprocess.STDOUT,
+                                                   encoding="utf-8", text=True)
+                    for line in process.stdout:
+                        # print(line)
+
+                        percent_res = re.search(r'\((?P<percent>[\s\S]*?)%\)', line)
+                        if percent_res is not None:
+                            progress = percent_res.groupdict()['percent']
+                            print(progress)
+                            self.progressBar["value"] = progress
+                            self.show_progress.set(progress + "%")
+                            self.update()
+                    process.wait()
+                    if process.poll() == 0:
+                        self.progressBar["value"] = 100
+                        self.show_progress.set("100%")
+                        self.show_screen.set("下载完成")
+                        self.update()
+                        kind = filetype.guess(down_path + down_name)
+                        if kind is None:
+                            print("文件类型不支持")
+                        else:
+                            name_len = len(down_name) - 5
+                            try:
+                                os.rename(down_path + down_name,
+                                          down_path + down_name[:name_len] + "." + kind.extension)
+                            except Exception as e:
+                                print(e)
+                                messagebox.showwarning(title="警告", message=str(e))
+                except Exception as e:
+                    print("网络未连接，请检查连接后重试")
+                    print(e)
+                    messagebox.showwarning(title="警告", message="网络未连接，请检查连接后重试")
+                ############
+            except Exception as e:
+                print("网络未连接，请检查连接后重试")
+                print(e)
+                messagebox.showwarning(title="警告", message="网络未连接，请检查连接后重试")
+                return
+        finally:
+            self.lock.release()
+
     def audio_down(self):
         music_url = self.entryUrl.get().strip('\n')
         if music_url == "" or music_url == "请输入分享链接即可":
             messagebox.showwarning(title="警告", message="请输入分享链接后执行")
             return
-        self.progressBar["value"] = 0
-        self.show_progress.set("")
-        self.show_screen.set(self.program_name)
-        self.update()
-        try:
-            music_data = Amusic.get_all_music_parm(music_url)
-            if music_data[0] == "null" and music_data[1] == "null":
-                print("不支持此分享链接或者链接格式错误")
-                messagebox.showwarning(title="警告", message="不支持此分享链接或者链接格式错误")
-                return
-            music_name = music_data[0]
-            music_play_url = music_data[1]
-            self.show_screen.set(music_name)
-            ###开始下载###
-            try:
-                if sys.platform[:3] == "win":
-                    down_path = "downloads/"
-                if sys.platform == "darwin":
-                    down_path = os.path.expanduser("~/Music/")
-                down_name = music_name + ".temp"
-                str_out = ['./aria2c', '-d', down_path, '-o', down_name, music_play_url]
-                print(str_out)
-                if sys.platform[:3] == "win":
-                    si = subprocess.STARTUPINFO()
-                    si.dwFlags = subprocess.CREATE_NEW_CONSOLE | subprocess.STARTF_USESHOWWINDOW
-                    si.wShowWindow = subprocess.SW_HIDE
-                    process = subprocess.Popen(str_out, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
-                                               encoding="utf-8",
-                                               text=True, startupinfo=si)
-                if sys.platform == "darwin":
-                    process = subprocess.Popen(str_out, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
-                                               encoding="utf-8", text=True)
-                for line in process.stdout:
-                    # print(line)
 
-                    percent_res = re.search(r'\((?P<percent>[\s\S]*?)%\)', line)
-                    if percent_res is not None:
-                        progress = percent_res.groupdict()['percent']
-                        print(progress)
-                        self.progressBar["value"] = progress
-                        self.show_progress.set(progress + "%")
-                        self.update()
-                process.wait()
-                if process.poll() == 0:
-                    self.progressBar["value"] = 100
-                    self.show_progress.set("100%")
-                    self.show_screen.set("下载完成")
-                    self.update()
-                    kind = filetype.guess(down_path + down_name)
-                    if kind is None:
-                        print("文件类型不支持")
-                    else:
-                        name_len = len(down_name) - 5
-                        try:
-                            os.rename(down_path + down_name, down_path + down_name[:name_len] + "." + kind.extension)
-                        except Exception as e:
-                            print(e)
-                            messagebox.showwarning(title="警告", message=str(e))
-            except Exception as e:
-                print("网络未连接，请检查连接后重试")
-                print(e)
-                messagebox.showwarning(title="警告", message="网络未连接，请检查连接后重试")
-            ############
-        except Exception as e:
-            print("网络未连接，请检查连接后重试")
-            print(e)
-            messagebox.showwarning(title="警告", message="网络未连接，请检查连接后重试")
-            return
+        if not self.lock.locked():
+            temp_thread = threading.Thread(target=self.down_thread, args=(music_url,))
+            temp_thread.setDaemon(True)
+            temp_thread.start()
 
     def del_url_show(self):
         self.entryUrl.delete(0, "end")
